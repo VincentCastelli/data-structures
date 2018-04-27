4c15169fd9b7202de9e4f32bbb08c218994acaf8
VincentCastelli <vincentsalvatorecastelli@gmail.com>
2018-04-26 18:04:30 -0700

    Passed the first four tests of linkedList.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,27 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+
+    if (this.head === null) { // if head is null
+      this.head = newNode; // assign head to value
+    } else {
+      this.head.next = newNode; // if head exists, assign newNode to next 
+    }
+
+    this.tail = newNode; // why does setting this.tail equal to newNode.value not work when you return this.tail?
+    return this.tail.value;
   };
 
   list.removeHead = function() {
+
+    if (this.head.next) { //if next exists  --> node5
+      this.head = this.head.next; //reassign head to next
+    } else { //else
+      this.head = null; //head = null
+      this.tail = null; //tail = null
+    }
+    return this.head.next;
   };
 
   list.contains = function(target) {
