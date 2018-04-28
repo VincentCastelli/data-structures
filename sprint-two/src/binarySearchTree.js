d2ac934f88021cd45658df8cfb250ff101512805
VincentCastelli <vincentsalvatorecastelli@gmail.com>
2018-04-27 21:32:14 -0700

    Finished first 2 tests for binary tree.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,58 @@
 var BinarySearchTree = function(value) {
+  var newBinaryTree = {};
+
+  newBinaryTree.value = value;  
+  newBinaryTree.left = null;
+  newBinaryTree.right = null;
+
+  _.extend(newBinaryTree, newBinaryTreeMethods);
+
+
+  return newBinaryTree;
+};
+
+
+
+var newBinaryTreeMethods = {};
+
+
+newBinaryTreeMethods.insert = function (value) {
+  var rootNode = this; 
+  var currentNode = rootNode;
+  
+  var newNode = {
+    value: value,
+    left: null,
+    right: null
+  };
+  
+  while (currentNode) {
+    if (newNode.value < currentNode.value) {
+      if (currentNode.left === null) {
+        currentNode.left = newNode;
+        break;
+      } else {
+        currentNode = currentNode.left;
+      }
+    }
+
+    if (newNode.value > currentNode.value) {
+      if (currentNode.right === null) {
+        currentNode.right = newNode;
+        break;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+  }
+};
+
+newBinaryTreeMethods.contains = function (target) {
+
+};
+
+newBinaryTreeMethods.depthFirstLog = function () {
+
 };
 
 
