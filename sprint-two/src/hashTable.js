6e3e4e57ac2870c47e2d4377c9a1e6270711ce14
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-04-29 13:43:02 -0700

    Finished hashTable.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,9 +1,9 @@
-var HashTable = function() {
+var HashTable = function () {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function (k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var newBucket = [[k, v]];
   var newTuple = [k, v];
@@ -14,11 +14,11 @@ HashTable.prototype.insert = function(k, v) {
       if (k === this._storage.get(index)[i][0]) {
         exists = true;
         this._storage.get(index)[i][1] = v;
-      } 
+      }
     }
 
     if (!exists) {
-      this._storage.get(index)[0].push(newTuple);
+      this._storage.get(index).push(newTuple);
     }
   } else {
     this._storage.set(index, newBucket);
@@ -26,26 +26,31 @@ HashTable.prototype.insert = function(k, v) {
 
 };
 
-HashTable.prototype.retrieve = function(k) {
+HashTable.prototype.retrieve = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit); // [0]
-  
-  // [['Steven', 'Segal']]
-  if (k === this._storage.get(index)[0][0]) {
-    return this._storage.get(index)[0][1];
-  }  
-  // return [bucket[1]]
-  console.log(this._storage.get(index));  
+
+  if (this._storage.get(index) !== undefined) {
+    for (var i = 0; i < this._storage.get(index).length; i++) {
+      if (k === this._storage.get(index)[i][0]) {
+        return this._storage.get(index)[i][1];
+      }
+    }
+  }
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.remove = function (k) {
 
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  if (this._storage.get(index) !== undefined) {
+    for (var i = 0; i < this._storage.get(index).length; i++) {
+      if (k === this._storage.get(index)[i][0]) {
+        this._storage.get(index)[i].pop();
+      }
+    }
+  }
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
